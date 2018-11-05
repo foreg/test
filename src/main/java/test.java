53d4abb6ca016e3ebf3d0bcd94d04d0826be294a
foreg <37451880+foreg@users.noreply.github.com>
2018-11-05 21:59:40 +0500

    Initial commit

---
+++ src/main/java/test.java
@@ -0,0 +1,11 @@
+
+
+abstract class test {
+
+    public static void main(String[] args){
+        Object a =null;
+        if (a!= null & (int)a > 0) {
+            System.out.println("dfh");
+        }
+    }
+}
